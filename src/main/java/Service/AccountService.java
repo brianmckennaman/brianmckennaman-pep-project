82b3d548c88b1f794d1c4f58d94049903a2ada05
Brian McKenna <brianmckennaman@gmail.com>
2023-05-12 00:13:44 +0000

    created dao & service files

---
+++ src/main/java/Service/AccountService.java
@@ -0,0 +1,17 @@
+package Service;
+
+import Model.Account;
+import DAO.AccountDAO;
+import java.util.List;
+
+public class AccountService {
+    private AccountDAO accountDAO;
+
+    public AccountService() {
+        accountDAO = new AccountDAO();
+    }
+
+    public AccountService(AccountDAO accountDAO){
+        this.accountDAO = accountDAO;
+    }
+}
---
+++ src/main/java/Service/MessageService.java
@@ -0,0 +1,17 @@
+package Service;
+
+import DAO.MessageDAO;
+import Model.Message;
+import java.util.List;
+
+public class MessageService {
+    public MessageDAO messageDAO;
+
+    public MessageService(){
+        messageDAO = new MessageDAO();
+    }
+
+    public MessageService(MessageDAO messageDAO){
+        this.messageDAO = messageDAO;
+    }
+}
